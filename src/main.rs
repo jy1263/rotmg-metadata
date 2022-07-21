86951601d76b43b22926829b3ecacf4e2b9a3c9d
amy <ctrlalternativedelete@gmail.com>
2022-07-21 17:17:00 +1000

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{path::{Path, PathBuf}, iter::Zip, io::{Cursor, self}, fs};
+use std::{fs, io, path::Path};
 
-use exalta_core::{download::{request_checksums, download_files_from_checksums}, misc::init};
+use exalta_core::{
+    download::{download_files_from_checksums, request_checksums},
+    misc::init,
+};
+use std::process::Command;
 use zip::ZipArchive;
 
 static ASSET_RIPPER_PLATFORM: &str = "linux_x64";
@@ -11,6 +15,7 @@ static OUT: &str = "./out";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let build_hash = init(None, None).await?.build_hash;
     let output_path = Path::new(OUT);
+    let ripper_output = output_path.join("ripper");
 
     println!("{}", build_hash);
 
@@ -23,58 +28,87 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // download_files_from_checksums(&build_hash, PLATFORM, &output_path.to_path_buf(), &checksums_files, None).await?;
 
+    let asset_ripper_path = output_path.join("AssetRipperConsole");
+    if !asset_ripper_path.exists() {
+        let octocrab = octocrab::instance();
+        let releases = octocrab
+            .repos("AssetRipper", "AssetRipper")
+            .releases()
+            .list()
+            .send()
+            .await?
+            .take_items();
 
-    let octocrab = octocrab::instance();
-    let releases = octocrab.repos("AssetRipper", "AssetRipper").releases().list().send().await?.take_items();
+        let unfound_err = "No Releases Found for AssetRipper";
+        let release = releases
+            .first()
+            .ok_or_else(|| anyhow::anyhow!(unfound_err))?;
+        let release_asset = release
+            .assets
+            .iter()
+            .find(|e| e.name.contains(ASSET_RIPPER_PLATFORM))
+            .ok_or_else(|| anyhow::anyhow!(unfound_err))?;
+        let buf = reqwest::get(release_asset.browser_download_url.clone())
+            .await?
+            .bytes()
+            .await?;
+        let mut zip = ZipArchive::new(std::io::Cursor::new(buf))?;
+        for i in 0..zip.len() {
+            let mut file = zip.by_index(i).unwrap();
+            let outpath = output_path.join(match file.enclosed_name() {
+                Some(path) => path.to_owned(),
+                None => continue,
+            });
 
-    let unfound_err = "No Releases Found for AssetRipper";
-    let release = releases.first().ok_or_else(|| anyhow::anyhow!(unfound_err))?;
-    let release_asset = release.assets.iter().find(|e| e.name.contains(ASSET_RIPPER_PLATFORM)).ok_or_else(|| anyhow::anyhow!(unfound_err))?;
-    let buf = reqwest::get(release_asset.browser_download_url.clone()).await?.bytes().await?;
-
-    let mut zip = ZipArchive::new(std::io::Cursor::new(buf))?;
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i).unwrap();
-        let outpath = match file.enclosed_name() {
-            Some(path) => path.to_owned(),
-            None => continue,
-        };
-
-        {
-            let comment = file.comment();
-            if !comment.is_empty() {
-                println!("File {} comment: {}", i, comment);
-            }
-        }
-
-        if (*file.name()).ends_with('/') {
-            println!("File {} extracted to \"{}\"", i, outpath.display());
-            fs::create_dir_all(&outpath).unwrap();
-        } else {
-            println!(
-                "File {} extracted to \"{}\" ({} bytes)",
-                i,
-                outpath.display(),
-                file.size()
-            );
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(&p).unwrap();
+            {
+                let comment = file.comment();
+                if !comment.is_empty() {
+                    println!("File {} comment: {}", i, comment);
                 }
             }
-            let mut outfile = fs::File::create(&outpath).unwrap();
-            io::copy(&mut file, &mut outfile).unwrap();
-        }
 
-        // Get and Set permissions
-        #[cfg(unix)]
-        {
-            use std::os::unix::fs::PermissionsExt;
+            if (*file.name()).ends_with('/') {
+                println!("File {} extracted to \"{}\"", i, outpath.display());
+                fs::create_dir_all(&outpath).unwrap();
+            } else {
+                println!(
+                    "File {} extracted to \"{}\" ({} bytes)",
+                    i,
+                    outpath.display(),
+                    file.size()
+                );
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(&p).unwrap();
+                    }
+                }
+                let mut outfile = fs::File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
 
-            if let Some(mode) = file.unix_mode() {
-                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            // Get and Set permissions
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::PermissionsExt;
+
+                if let Some(mode) = file.unix_mode() {
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                }
             }
         }
+    }
+    #[cfg(unix)]
+    {
+        println!(
+            "{}",
+            Command::new("chmod")
+                .args([
+                    "+x",
+                    &output_path.join("AssetRipperConsole").to_str().unwrap()
+                ])
+                .output()?
+                .status
+        );
     }
 
     Ok(())
